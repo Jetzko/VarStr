a8e359318623fd0bf5df079063ec5163858d4939
Jetzko <andreadefilippi68@gmail.com>
2023-01-27 10:21:28 +0100

    Variables and String

---
+++ src/main/kotlin/Fun.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val number = 15;
+    val numberF = 7.97f;
+    val string = "Piero";
+    val stringNumber = "5";
+
+    println("number : $number; numberF : $numberF, string : $string, stringNumber : $stringNumber");
+}
